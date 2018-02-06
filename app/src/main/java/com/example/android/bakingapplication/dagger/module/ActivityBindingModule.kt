86f70d54ab783b960f67380cc87fb2ac6b417802
Steven Gerdes <steven.gerdes@tackmobile.com>
2018-02-06 04:09:55 -0600

    Remove boilerplate and use @ContributesAndroidInjector

--- app/src/main/java/com/example/android/bakingapplication/dagger/module/ActivityBindingModule.kt
+++ app/src/main/java/com/example/android/bakingapplication/dagger/module/ActivityBindingModule.kt
@@ -1,20 +1,12 @@
 package com.example.android.bakingapplication.dagger.module
 
-import android.app.Activity
-import com.example.android.bakingapplication.dagger.component.MainActivitySubcomponent
 import com.example.android.bakingapplication.view.activity.MainActivity
-import dagger.Binds
 import dagger.Module
-import dagger.android.ActivityKey
-import dagger.android.AndroidInjector
-import dagger.multibindings.IntoMap
+import dagger.android.ContributesAndroidInjector
 
-@Module(subcomponents = [MainActivitySubcomponent::class])
+@Module
 abstract class ActivityBindingModule  {
 
-    @Binds
-    @IntoMap
-    @ActivityKey(MainActivity::class)
-    abstract fun bindMainActivityInjectorFactory(builder:  MainActivitySubcomponent.Builder):
-            AndroidInjector.Factory<out Activity>
+    @ContributesAndroidInjector
+    abstract fun bindMainActivityInjector(): MainActivity
 }
